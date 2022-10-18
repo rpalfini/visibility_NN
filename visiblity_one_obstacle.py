8052c92f24a7f480ec6d5694425ba615dbf107f8
Robert <rpalfini@uci.edu>
2022-10-17 20:31:07 -0700

    WindowsInhibitor stops sleep during data gen

--- visiblity_one_obstacle.py
+++ visiblity_one_obstacle.py
@@ -1,14 +1,24 @@
 from visibility_graph import *
 import time
+import WindowsInhibitor as wi #prevents computer from sleeping while generating data
+
+osSleep = None
+
+if os.name == 'nt':
+    osSleep = wi.WindowsInhibitor()
+    osSleep.inhibit()
+
 # start_vals = [(3,9)]
 start_vals = [(3,9),(7,17)]
 # end_vals = [(23,16)]
 # radius1 = 1 #obstacle radius
 # end_vals = [(23,9)]
 end_vals = [(23,9),(19,5)]
-radius1 = 5
+# radius1 = 5
+radius1 = 6
 
-obst_locations = [(13,10)]
+# obst_locations = [(13,10)]
+obst_locations = [(18,24)]
 
 #TODO what does specifying the dtype=object when creating the ndarray
 # trying out large data file
@@ -24,11 +34,11 @@ obstacle_list = init_obs(obst_locations,radius1)
 
 # Trying batch start/end vals
 tol = 0.01
-num_points = 40
+num_points = 60
 min_x,max_x = find_test_range(obstacle_list)
-start_x = np.linspace(-10,min_x-tol,num_points) # could try using np.arange
+start_x = np.linspace(min_x-tol-50,min_x-tol,num_points) # could try using np.arange
 start_y = np.linspace(-10,50,num_points)
-end_x = np.linspace(max_x+tol,50,num_points)
+end_x = np.linspace(max_x+tol,max_x+tol+50,num_points)
 end_y = start_y
 # start_x = np.linspace(-10,min_x-tol,num_points)
 # start_y = np.linspace(-10,50,num_points)
@@ -54,10 +64,11 @@ print(f"created the data in {toc - tic:0.4f} seconds")
 # vis_graph_one_obst.plot_full_vis_graph(0,"env 1_0")
 # vis_graph_one_obst.show_graph()
 
-vis_graph_one_obst.output_csv('2022_10_17one_obst data_large')
+vis_graph_one_obst.output_csv('2022_10_17one_obst data_large_5')
 # vis_graph_one_obst.save_plot_image('sol_r=1')
 
-
+if osSleep:
+    osSleep.uninhibit()
 # plt.show()
 
 

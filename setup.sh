d81b2a30ca15d92f5b2bc90e05c3169792217b8e
Robert <rpalfini@uci.edu>
2023-02-19 14:13:04 -0800

    updating shell scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # cd visibility_NN/
 pip install numpy matplotlib pandas bidict
-python obstacle_course_gen.py -f 23_02_18_aws_batch9 -nc 5 -no 20
+python obstacle_course_gen.py -f 23_02_19_aws_batch2 -nc 5 -no 20

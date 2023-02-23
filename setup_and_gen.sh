2eba535143b41de724a9de6ecc6cfb3ea907eca5
Robert <rpalfini@uci.edu>
2023-02-23 14:50:09 -0800

    readded obstacle course gen

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -3,6 +3,8 @@ batch_num=6
 today=$(date '+%y_%m_%d')
 file_name=${today}_aws_batch${batch_num}
 
+python3 obstacle_course_gen.py -f $file_name -nc 5 -no 20
+
 for ii in {0..19}
 do
     test_file=${file_name}_${ii}.txt

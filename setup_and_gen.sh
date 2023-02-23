6a9c4d0f9e037cf2f9c60e0f76a32c8aa1112311
Robert <rpalfini@uci.edu>
2023-02-23 14:56:08 -0800

    fixed error

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -8,7 +8,7 @@ python3 obstacle_course_gen.py -f $file_name -nc 5 -no 20
 for ii in {0..19}
 do
     test_file=${file_name}_${ii}.txt
-    log_file=${file_name}.log
-    error_file=${file_name}_error.log
-    python3 vis_main.py test_file -b True -gs -f $file_name > $log_file > $error_file &
+    log_file=./run_logs/${file_name}.log
+    error_file=./run_logs/${file_name}_error.log
+    python3 vis_main.py $test_file -b True -gs -f $file_name > $log_file > $error_file &
 done

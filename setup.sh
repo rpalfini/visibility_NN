5ac1020ae9eaddb688727b4bb1009330e43b68e3
Robert <rpalfini@uci.edu>
2023-02-22 22:09:41 -0800

    removing pip install from setup and gen

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# cd visibility_NN/
 pip install numpy matplotlib pandas bidict
-python3 obstacle_course_gen.py -f 23_02_19_aws_batch2 -nc 5 -no 20
--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-pip install numpy matplotlib pandas bidict
-python3 obstacle_course_gen.py -f 23_02_22_aws_batch4 -nc 5 -no 20
+python3 obstacle_course_gen.py -f 23_02_22_aws_batch5 -nc 5 -no 20
 
 for ii in {0..19}
 do
-    python3 vis_main.py 23_02_22_aws_batch4_$ii.txt -b True -gs -f 23_02_22_aws_batch4 &
+    python3 vis_main.py 23_02_22_aws_batch5_$ii.txt -b True -gs -f 23_02_22_aws_batch5 &
 done

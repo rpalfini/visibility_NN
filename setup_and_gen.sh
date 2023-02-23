211c47df0cb747cbfc9f4fd46d9ac508d2029733
Robert <rpalfini@uci.edu>
2023-02-22 20:32:16 -0800

    updated date

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 pip install numpy matplotlib pandas bidict
-python3 obstacle_course_gen.py -f 23_02_19_aws_batch2 -nc 5 -no 20
+python3 obstacle_course_gen.py -f 23_02_22_aws_batch4 -nc 5 -no 20
 
 for ii in {0..19}
 do
-    python3 vis_main.py 23_02_19_aws_batch2_$ii.txt -b True -gs &
+    python3 vis_main.py 23_02_22_aws_batch4_$ii.txt -b True -gs &
 done

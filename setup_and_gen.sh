e2cc1e3a225074dd9f00f21315d889c34a167733
Robert <rpalfini@uci.edu>
2023-02-22 21:38:45 -0800

    updated auto shell script

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -5,5 +5,5 @@ python3 obstacle_course_gen.py -f 23_02_22_aws_batch4 -nc 5 -no 20
 
 for ii in {0..19}
 do
-    python3 vis_main.py 23_02_22_aws_batch4_$ii.txt -b True -gs &
+    python3 vis_main.py 23_02_22_aws_batch4_$ii.txt -b True -gs -f 23_02_22_aws_batch4 &
 done

bc2f7b46eb1ae23a0bcb65b5ef29ea4a7527b759
Robert <rpalfini@uci.edu>
2023-02-28 13:06:39 -0800

    added log_dir creation

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -22,12 +22,22 @@ prefix=$file_name
 # Count the number of files that match the prefix
 num_files=$(ls -1 "${dir}/${prefix}_"* 2>/dev/null | wc -l)
 
+# create log file directory
+log_dir="./run_logs"
+
+if [ ! -d "$log_dir" ]; then
+    mkdir "$log_dir"
+    echo "Directory created: $log_dir"
+else
+    echo "Directory already exists: $log_dir"
+fi
+
 # Run a for loop for each file that matches the prefix
 for (( ii=1; ii<=num_files; ii++ ))
 do
     test_file=${file_name}_${ii}.txt
-    log_file=./run_logs/${file_name}.log
-    error_file=./run_logs/${file_name}_error.log
+    log_file=${log_dir}/${file_name}.log
+    error_file=${log_dir}/${file_name}_error.log
     python3 vis_main.py "$test_file" -b True -gs -f "$file_name" >> "$log_file" 2>> "$error_file" &
 done
 

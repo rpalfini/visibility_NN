049cdeb6e214d56207faa373d7e2c7bc3e7c90af
Robert <rpalfini@uci.edu>
2023-02-28 13:32:21 -0800

    adding debug msg

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -39,6 +39,9 @@ do
     log_file=${log_dir}/${file_name}.log
     error_file=${log_dir}/${file_name}_error.log
     python3 vis_main.py "$test_file" -b True -gs -f "$file_name" >> "$log_file" 2>> "$error_file" &
+    echo "test_file = $test_file"
+    echo "file_name = $file_name"
+    echo "ii = $ii"
 done
 
 echo "num obs files found: $num_files"

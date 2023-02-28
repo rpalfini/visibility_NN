583f47faab995e90324b190db8a9e74635d8b862
Robert <rpalfini@uci.edu>
2023-02-28 11:15:44 -0800

    updated setup_and gen and added settings file to make sure line endings on bash script are correct

--- setup_and_gen.sh
+++ setup_and_gen.sh
@@ -13,12 +13,29 @@ done
 echo "$batch_num"
 file_name="${file_name_prefix}${batch_num}"
 
-python3 obstacle_course_gen.py -f $file_name -nc 5 -no 20
+python3 obstacle_course_gen.py -f "$file_name" -nc 5 -no 20
 
-for ii in {0..19}
+# Set the prefix to search for
+dir=$
+prefix=$file_name
+
+# Count the number of files that match the prefix
+num_files=$(ls -1 "${dir}/${prefix}"* 2>/dev/null | wc -l)
+
+# Run a for loop for each file that matches the prefix
+for (( ii=1; ii<=num_files; ii++ ))
 do
     test_file=${file_name}_${ii}.txt
     log_file=./run_logs/${file_name}.log
     error_file=./run_logs/${file_name}_error.log
-    python3 vis_main.py $test_file -b True -gs -f $file_name >> $log_file 2>> $error_file &
+    python3 vis_main.py "$test_file" -b True -gs -f "$file_name" >> "$log_file" 2>> "$error_file" &
 done
+
+
+# for ii in {0..19}
+# do
+#     test_file=${file_name}_${ii}.txt
+#     log_file=./run_logs/${file_name}.log
+#     error_file=./run_logs/${file_name}_error.log
+#     python3 vis_main.py $test_file -b True -gs -f $file_name >> $log_file 2>> $error_file &
+# done

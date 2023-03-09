f9909228fc52ec45b6f2cf796f64ddf8844553fd
Robert <rpalfini@uci.edu>
2023-03-09 10:26:11 -0800

    fixed not all full patyhs being entered

--- full_path_setup_and_gen.sh
+++ full_path_setup_and_gen.sh
@@ -37,9 +37,9 @@ fi
 # Run a for loop for each file that matches the prefix
 for (( ii=0; ii<num_files; ii++ ))
 do
-    test_file=${file_name}_${ii}.txt
-    log_file=${log_dir}/${file_name}.log
-    error_file=${log_dir}/${file_name}_error.log
+    test_file=$base_path/${file_name}_${ii}.txt
+    log_file=$base_path/${log_dir}/${file_name}.log
+    error_file=$base_path/${log_dir}/${file_name}_error.log
     python3 vis_main.py "$test_file" -b True -gs -f "$file_name" >> "$log_file" 2>> "$error_file" &
     echo "test_file = $test_file"
     echo "file_name = $file_name"

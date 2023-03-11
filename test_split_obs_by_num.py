05f87b8cf6d3d46b8a1753b6f3924c8167762e15
Robert <rpalfini@uci.edu>
2023-03-10 18:57:52 -0800

    Got obstacle file split code working with no pad results

--- test_split_obs_by_num.py
+++ test_split_obs_by_num.py
@@ -3,33 +3,67 @@ import os
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import math
 
-
-def determine_num_obs(line,max_num_obs,end_idx=3):
-    '''determines number of obstacles by counting how many sets of 0,0,0 are on the input starting from last place of obstacles to first place of obstacles in data file'''
-    max_num_obs = 20
+def divide_line_by_entries(line):
+    '''divides comma separated file and converts entries from string to float'''
     tokens = line.split(',')
     tokens = list(map(float,tokens))
-    start_idx = end_idx+max_num_obs*3
+    return tokens
+
+def find_first_label_idx(max_num_obs,end_idx):
+    first_label_idx = end_idx+max_num_obs*3+1
+    return first_label_idx
+
+def determine_num_obs(line,max_num_obs,end_idx):
+    '''determines number of obstacles by counting how many sets of 0,0,0 are on the input starting from last place of obstacles to first place of obstacles in data file'''
+    tokens = divide_line_by_entries(line)
+    start_idx = find_first_label_idx(max_num_obs,end_idx)-1
     zero_counter = 0
     for ii in range(start_idx,end_idx,-1):
         if tokens[ii] == 0:
             zero_counter += 1
         else:
             # if we encounter a non zero that means we are done with place holder obstacles
+            null_obs_start_idx = ii+1
+            null_obs_end_idx = start_idx+1
+            null_obs_idx = (null_obs_start_idx,null_obs_end_idx)
             break
+    
     num_empty_obs = math.floor(zero_counter/3)
     num_obs = max_num_obs - num_empty_obs
     print(f'num_empty_obs = {num_empty_obs}')
-    print(f'num_empty_obs % 3 = {num_empty_obs%3}')
+    print(f'zero_counter % 3 = {zero_counter%3}')
     print(f'num_obs = {num_obs}')
+    return num_obs, null_obs_idx
 
-    return num_obs
+def find_null_label_idx(num_obs,max_num_obstacles,end_idx):
+    first_label_idx = find_first_label_idx(max_num_obstacles,end_idx)
+    # num_empty_obs = max_num_obstacles-num_obs
+    first_null_label_idx = first_label_idx + num_obs
+    end_null_label_idx = first_label_idx + max_num_obstacles
+    null_label_idx = (first_null_label_idx,end_null_label_idx)
+    return null_label_idx
 
-def resize_row():
-    pass
+def resize_line(line,num_obs,max_num_obstacles,null_obs_idx,null_label_idx):
+    '''Use to remove the empty obstacle entries and resize data'''
+    if num_obs < max_num_obstacles:
+        tokens = divide_line_by_entries(line) 
+        del tokens[null_label_idx[0]:null_label_idx[1]] # delete null labels
+        del tokens[null_obs_idx[0]:null_obs_idx[1]] # delete null obstacles
+        expected_num_tokens = 4+num_obs*3+num_obs+1
+        actual_num_tokens = len(tokens)
+        if actual_num_tokens != expected_num_tokens:
+            raise Exception('new line has incorrect number of tokens')
+        new_line = ','.join(str(x) for x in tokens)
+    elif num_obs == max_num_obstacles:
+        new_line = line
+    else:
+        raise Exception('num_obstacles > max_num_obstacles')
+    return new_line
 
 if __name__ == "__main__":
-    resize = True
+    resize = True # makes outputted files not have the padded obstacles and labels to make the data fit the 20 size framework
+    max_num_obs = 20
+    first_obstacle_idx = 4
     fname = "single_course_set_merge.csv"
     fname_no_extension = os.path.splitext(fname)[0]
     # create a dictionary of file handles for the 20 output files
@@ -42,9 +76,11 @@ if __name__ == "__main__":
     file_path = os.path.join("./results_merge",fname)
     data_gen = cfc.csv_reader(file_path)
     for row in data_gen:
-        num_obs = determine_num_obs(row)
+        num_obs, null_obs_idx = determine_num_obs(row,max_num_obs,first_obstacle_idx-1)
         if resize:
-            new_row = resize_row(row)
+            null_label_idx = find_null_label_idx(num_obs,max_num_obs,first_obstacle_idx-1)
+            new_row = resize_line(row,num_obs,max_num_obs,null_obs_idx,null_label_idx)
+            output_files[num_obs].write(new_row)
         else:
             output_files[num_obs].write(row)
 

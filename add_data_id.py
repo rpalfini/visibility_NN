3243fcdd8ca29161d28957cb9274bdafc797e83e
Robert <rpalfini@uci.edu>
2023-04-06 13:55:58 -0700

    code for using generators

---
+++ add_data_id.py
@@ -0,0 +1,22 @@
+import csv_file_combiner as cfc
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    fname = "./ML_code/data/main_data_file_courses1.csv"
+    output_fname = "./ML_code/data/23_03_15_main_data_file_courses1_id.csv"
+
+    f = open(output_fname,"w")
+    data_gen_in = cfc.csv_reader(fname)
+    ii = 0
+    for row in data_gen_in:
+        tokens = row.split(",")
+        tokens.append(str(ii))
+        out_row = ",".join(tokens)
+        f.write(out_row)
+        ii += 1
+    f.close()

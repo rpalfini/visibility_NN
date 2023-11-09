b52443d96cb0d33c08277c66817164f2c7bdf8f7
Robert <rpalfini@uci.edu>
2023-11-09 15:55:06 -0800

    updated to run when data splits are unknown

--- ML_code/evaluate_keras_model.py
+++ ML_code/evaluate_keras_model.py
@@ -8,28 +8,44 @@ def main(model_path,epoch,data_file,num_obs):
     # This function lets us load a model with specific weights and evaluate on a new dataset
     weight_loaded_model = load_model_with_checkpoint(model_path,epoch)
     
-    split_percentages = [0.9, 0.05, 0.05]
-    dataset = util.load_data(data_file)
-    split_data = util.shuffle_and_split_data(dataset,num_obs,split_percentages,shuffle_data=False)
-    X_train = split_data["X_train"] 
-    X_val = split_data["X_val"]   
-    X_test = split_data["X_test"]  
-
-    Y_train = split_data["Y_train"] 
-    Y_val = split_data["Y_val"]   
-    Y_test = split_data["Y_test"]  
-
-    # evaluate the keras weight_loaded_model
-    print('testing training data')
-    _, train_accuracy = weight_loaded_model.evaluate(X_train, Y_train)
-    print('testing validation data')
-    _, val_accuracy = weight_loaded_model.evaluate(X_val, Y_val)
-    print('testing test data')
-    _, test_accuracy = weight_loaded_model.evaluate(X_test, Y_test)
-    print('Train_Accuracy: %.2f' % (train_accuracy*100))
-    print('Validation_Accuracy: %.2f' % (val_accuracy*100))
-    print('Test_Accuracy: %.2f' % (test_accuracy*100))
     
+    is_shift_data = True # this shifts the data over so that the course is center around origin of (0,0)
+    is_split_data = False
+    if is_split_data:
+        print('splitting data')
+        split_percentages = {"train": 0.9, "val": 0.05, "test": 0.05}
+        dataset_in = util.load_data(data_file)
+        dataset_processed = util.shift_data_set(dataset_in,num_obs,is_shift_data)
+        split_data = util.shuffle_and_split_data(dataset_processed,args.num_obs,split_percentages)
+        # split_data = util.shuffle_and_split_data(dataset,num_obs,split_percentages,shuffle_data=False)
+        X_train = split_data["X_train"] 
+        X_val = split_data["X_val"]   
+        X_test = split_data["X_test"]  
+
+        Y_train = split_data["Y_train"] 
+        Y_val = split_data["Y_val"]   
+        Y_test = split_data["Y_test"]  
+
+        # evaluate the keras weight_loaded_model
+        print('testing training data')
+        _, train_accuracy = weight_loaded_model.evaluate(X_train, Y_train)
+        print('testing validation data')
+        _, val_accuracy = weight_loaded_model.evaluate(X_val, Y_val)
+        print('testing test data')
+        _, test_accuracy = weight_loaded_model.evaluate(X_test, Y_test)
+        print('Train_Accuracy: %.2f' % (train_accuracy*100))
+        print('Validation_Accuracy: %.2f' % (val_accuracy*100))
+        print('Test_Accuracy: %.2f' % (test_accuracy*100))
+    else:
+        print('not splitting data')
+        dataset_in = util.load_data(data_file)
+        dataset_processed = util.shift_data_set(dataset_in,num_obs,is_shift_data)
+        X, Y = util.separate_features_labels(dataset_processed,num_obs)
+        print('testing all data')
+        loss, accuracy = weight_loaded_model.evaluate(X,Y)
+        print('Accuracy: %.2f' % (accuracy*100))
+        print(f'Loss: {loss}')
+
 
 def load_model_with_checkpoint(model_path,epoch):
     # makes model load work if results directory or keras model directory is inputted
@@ -41,6 +57,7 @@ def load_model_with_checkpoint(model_path,epoch):
 
     if os.name == 'nt':
         dir_path = dir_path.replace('/','\\')
+        print(dir_path)
 
     loaded_model = K.models.load_model(model_path)
     checkpoint_template = util.make_checkpoint_template()
@@ -55,7 +72,7 @@ def arg_parse():
     parser = ArgumentParser(description="Script allows you to load and test a model with weights from earlier epoch.",formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-n", "--num_obs", type=int, default = 3, help="Specify number of obstacles in selected data set")
     parser.add_argument("-d", "--data_path", type=str, default = "./ML_code/Data/small_main_data_file_courses3.csv", help = 'Path to dataset you want to evaluate NN model on.')
-    parser.add_argument("-m","--model_path", type=str, default = "./main_train_results/small_main_data_file_courses3/model_15", help = 'Path to model results you wish to pull weights from.')
+    parser.add_argument("-f","--model_path", type=str, default = "./main_train_results/small_main_data_file_courses3/model_15", help = 'Path to model results you wish to pull weights from.')
     parser.add_argument("-e","--epoch", type=int, default=2, help="Chooses the weights from a given epoch to be loaded into the model")
     
     args = parser.parse_args()

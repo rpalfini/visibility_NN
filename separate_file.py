049350cac397fc0dc80471f11b7f8b3129429af7
Robert <rpalfini@uci.edu>
2023-11-10 15:06:20 -0800

    added input for number of lines to chop file at

--- separate_file.py
+++ separate_file.py
@@ -1,12 +1,14 @@
 
-large_file = "./ML_code/Data/main_data_file_courses3.csv"
-small_file = "./ML_code/Data/small_main_data_file_courses3.csv"
+large_file = "D:/Vis_network_data/data_file_by_course/main_data_file_courses20.csv"
+small_file = "./ML_code/Data/small_main_data_file_courses20.csv"
+# num_lines = 25000
+num_lines = 250
 with open(large_file, 'r') as source_file:
     # Open a new CSV file in write mode
     with open(small_file, 'w') as new_file:
         # Loop through the first 25,000 lines
         for line_number, line in enumerate(source_file):
-            if line_number < 25000:
+            if line_number < num_lines:
                 # Write the line to the new file
                 new_file.write(line)
             else:

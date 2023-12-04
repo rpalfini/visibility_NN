31654f41d035bac4a5e699a02b2e58bc6d18423d
Robert <rpalfini@uci.edu>
2023-12-03 20:26:49 -0800

    working on this file for adding data ids to my data

--- add_data_id.py
+++ add_data_id.py
@@ -7,8 +7,10 @@ import csv_file_combiner as cfc
 
 
 if __name__ == "__main__":
-    fname = "./ML_code/data/main_data_file_courses1.csv"
-    output_fname = "./ML_code/data/23_03_15_main_data_file_courses1_id.csv"
+    # fname = "./ML_code/data/main_data_file_courses1.csv"
+    # output_fname = "./ML_code/data/23_03_15_main_data_file_courses1_id.csv"
+    fname = "./ML_code/data/small_main_data_file_courses3.csv"
+    output_fname = "./ML_code/data/small_main_data_file_courses3_id.csv"
 
     f = open(output_fname,"w")
     data_gen_in = cfc.csv_reader(fname)

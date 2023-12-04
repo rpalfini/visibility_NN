f5dad02fbae6370c8f0e73ae97070ebe2c2a8abd
Robert <rpalfini@uci.edu>
2023-12-03 20:19:36 -0800

    new utitlity scripts for working with data files and finding number of obstacle courses in a data file.

---
+++ ML_code/find_index_courses_change.py
@@ -0,0 +1,69 @@
+import os
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import numpy as np
+
+import config # needed to load modules from other local packages
+import csv_file_combiner as cfc
+# from ML_code import util
+import util
+import save_csv_to_numpy as scn
+
+'''finds index where courses change as well as outputs those rows to file and tells youthe number of samples per data course'''
+def main():
+
+    # Assuming you have your data as a NumPy array
+    # Replace the following array with your actual data
+    # data = util.load_data("D:/Vis_network_data/data_file_by_course/main_data_file_courses20.npy")
+    data = util.load_data("E:/main_folder/npy_to_be_added_by_course/main_data_file_courses20.npy")
+    # data = util.load_data("ML_code/Data/small_main_data_file_courses20.csv")
+
+    # Define the columns you're interested in
+    columns_of_interest = np.arange(4,64)
+
+    # Use np.diff to find changes between consecutive rows in the specified columns
+    changes = np.diff(data[:, columns_of_interest], axis=0)
+
+    # Find rows where changes occur by checking if any element in the resulting array is non-zero
+    rows_with_changes = np.any(changes != 0, axis=1)
+
+    # Get the rows where values change
+    result = data[1:][rows_with_changes]
+
+    # Get the row indices where values change
+    result_indices = np.where(rows_with_changes)[0] + 1  # Adding 1 to account for zero-based indexing
+
+    index_differences = np.diff(result_indices)
+
+    num_points = 0
+    for i in range(len(index_differences) - 1, -1, -1):
+        # print(index_differences[i])
+        if num_points >= 792265:
+            print(f'last index was {i}.')
+            print(f'num_points needed {num_points}')
+            print(f'total courses - last index = {index_differences.shape[0] - i}')
+            break
+        
+        num_points += index_differences[i]
+
+    # Print or use the result array as needed
+    print(result)
+    print(result_indices)
+    print(index_differences)
+
+    output_file_path = "change_indices_20_obs_file"
+    with open(output_file_path, 'w') as file:
+        for index in result_indices:
+            file.write(f"{index}\n")
+
+    np.savetxt('new_data_20_obs_row_changed.txt',result,delimiter=',')
+
+
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- save_csv_to_numpy.py
+++ save_csv_to_numpy.py
@@ -6,15 +6,22 @@ from ML_code import util
 
 
 def main(args):
-
     if args.file_dir is None:
         raise Exception('To use directory mode, you need to specify a directory to search for csv files with -f option.')
     csv_files = cfc.get_file_list(args.file_dir)
     for fname in csv_files:
-        data = util.load_data(os.path.join(args.file_dir,fname))
-        file_save_path = os.path.join(args.save_path,f"main_data_file_courses{num_obs}.npy")
-        print(f'saving data to {file_save_path}.')
-        np.save(file_save_path,data)
+        if fname == "main_data_file_courses20.csv":
+            continue
+        load_and_save(args.save_path,args.file_dir,fname)
+
+def load_and_save(save_path,file_dir,fname):
+    '''Loads file located at file_dir/fname and saves data as .npy file'''
+    file_path = os.path.join(file_dir,fname)
+    data = util.load_data(file_path)
+    num_obs = util.read_model_num_from_file_path(file_path)
+    file_save_path = os.path.join(save_path,f"main_data_file_courses{num_obs}.npy")
+    print(f'saving data to {file_save_path}.')
+    np.save(file_save_path,data)
 
 def arg_parse():
     parser = ArgumentParser(description="Scripts converts all files in directory from csv to numpy and saves in new directory.",formatter_class=ArgumentDefaultsHelpFormatter)
@@ -29,3 +36,4 @@ def arg_parse():
 if __name__ == "__main__":
     args = arg_parse()
     main(args)
+    
